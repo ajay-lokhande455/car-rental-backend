6e7c9fe9bd7ffc207ecf22ea96f87b2c37d49311
Ajay Lokhande <lokhandeajay455@gmail.com>
2025-02-27 10:08:47 +0530

    changed

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ connectDB();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
-
 app.use(cors({
     origin: "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
