3a7a47df88d0d301cf1b4a0819f76a83487c2586
Ajay Lokhande <lokhandeajay455@gmail.com>
2025-02-27 10:00:52 +0530

    updated

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const connectDB = require('./src/config/db');
 dotenv.config();
 connectDB();
 
+app.use(cors());
+
 app.use(cors({
     origin: "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -18,7 +20,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to car rental app....' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
